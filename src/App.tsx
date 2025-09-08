109b2faf23fe99bb9565c32dd87291f38f31797c
PresidentTree94 <karlysams1218@gmail.com>
2025-09-08 14:46:39 -0400

    Updated CSS to Tailwind v4.

--- src/App.tsx
+++ src/App.tsx
@@ -83,12 +83,12 @@ function App() {
   }, []);
 
   return (
-    <main>
-      <h1>QR Code Generator</h1>
-      <section id="code" style={{backgroundColor: bgColor}} ref={ref}>
+    <main className="bg-component-bg p-8 m-8 rounded-2xl shadow-xl flex flex-col tl:grid tl:grid-cols-2 gap-8">
+      <h1 className="text-header-fg text-3xl font-bold col-span-full text-center">QR Code Generator</h1>
+      <section className="aspect-square flex justify-center rounded-2xl overflow-hidden" style={{backgroundColor: bgColor}} ref={ref}>
         <QRCode value={qrValue} level="M" bgColor={bgColor} fgColor={fgColor} style={{width: `${qrSize}%`, height: "auto"}} />
       </section>
-      <section id="custom">
+      <section className="tl:aspect-square flex flex-col gap-4">
         <div className="criteria">
           <label>Enter text or URL:</label>
           <input type="text" value={qrValue} onChange={(e) => setQrValue(e.target.value)} />
@@ -96,18 +96,18 @@ function App() {
         <div className="share">
           <div className="criteria">
             <label>Foreground:</label>
-            <input type="color" value={fgColor} onChange={(e) => setFgColor(e.target.value)} />
+            <input type="color" className="h-10 rounded-lg border border-solid border-page-bg bg-gray-200" value={fgColor} onChange={(e) => setFgColor(e.target.value)} />
           </div>
           <div className="criteria">
             <label>Background:</label>
-            <input type="color" value={bgColor} onChange={(e) => setBgColor(e.target.value)} />
+            <input type="color" className="h-10 rounded-lg border border-solid border-page-bg bg-gray-200" value={bgColor} onChange={(e) => setBgColor(e.target.value)} />
           </div>
         </div>
         <div className="criteria">
           <label>Size:</label>
           <div className="share">
-            <input type="range" value={qrSize} min={50} max={100} onChange={(e) => setQrSize(Number(e.target.value))} />
-            <input type="number" value={qrSize} min={50} max={100} onChange={(e) => setQrSize(Number(e.target.value))} />
+            <input className="appearance-reset" type="range" value={qrSize} min={50} max={100} onChange={(e) => setQrSize(Number(e.target.value))} />
+            <input type="number" className="w-16" value={qrSize} min={50} max={100} onChange={(e) => setQrSize(Number(e.target.value))} />
             <span>%</span>
           </div>
         </div>
@@ -116,27 +116,29 @@ function App() {
           <input type="text" value={fileName} onChange={(e) => setFileName(e.target.value)} />
         </div>
       </section>
-      <section id="buttons">
-        <button onClick={handleSave} disabled={!qrValue.trim() || !fileName.trim()}>Save</button>
-        <button onClick={handleDownload} disabled={!qrValue.trim() || !fileName.trim()}>Download</button>
-        <button onClick={resetCustomization}>Reset</button>
-        <button onClick={handleDelete} disabled={savedQrCodes.length === 0}>Delete</button>
+      <section id="buttons" className="flex flex-col gap-4">
+        <button className="bg-text-fg" onClick={handleSave} disabled={!qrValue.trim() || !fileName.trim()}>Save</button>
+        <button className="bg-accent-bg" onClick={handleDownload} disabled={!qrValue.trim() || !fileName.trim()}>Download</button>
+        <button className="bg-header-fg" onClick={resetCustomization}>Reset</button>
+        <button className="bg-text-fg" onClick={handleDelete} disabled={savedQrCodes.length === 0}>Delete</button>
       </section>
       {savedQrCodes.length === 0 ? (
-        <section id="load" style={{justifyContent: "center", alignItems: "center"}}>
+        <section id="load" className="bg-disable-bg rounded-lg flex justify-center items-center h-60">
           <p>No QR codes saved.</p>
         </section>
       ) : (
-        <section id="load" style={{gap: "0.5rem"}}>
-          {savedQrCodes.map((item, index) => (
-            <button key={index} onClick={() => {
-              setQrValue(item.text);
-              setFgColor(item.foregroundColor);
-              setBgColor(item.backgroundColor);
-              setQrSize(item.size);
-              setFileName(item.filename);
-            }}>{item.filename}</button>
-          ))}
+        <section id="load" className="bg-disable-bg rounded-lg p-2">
+          <div className="flex flex-col gap-2 max-tl:min-h-56 tl:h-56 tl:overflow-auto">
+            {savedQrCodes.map((item, index) => (
+              <button className="bg-component-bg p-1 text-left text-header-fg" key={index} onClick={() => {
+                setQrValue(item.text);
+                setFgColor(item.foregroundColor);
+                setBgColor(item.backgroundColor);
+                setQrSize(item.size);
+                setFileName(item.filename);
+              }}>{item.filename}</button>
+            ))}
+          </div>
         </section>
       )}
     </main>
